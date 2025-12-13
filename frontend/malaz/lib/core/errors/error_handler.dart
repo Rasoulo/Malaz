bc528b301b3b4b66af161bd8b67fbcb07b8a024b
LONYCODE <muhammadalhamwi963@gmail.com>
2025-12-14 01:26:01 +0300

    complete error handler

---
+++ frontend/malaz/lib/core/errors/error_handler.dart
@@ -0,0 +1,62 @@
+import 'package:dio/dio.dart';
+import '../errors/exceptions.dart';
+
+/// TODO: translate
+class ErrorHandler {
+  static Exception handle(DioException error) {
+    switch (error.type) {
+      case DioExceptionType.connectionTimeout:
+      case DioExceptionType.sendTimeout:
+      case DioExceptionType.receiveTimeout:
+      case DioExceptionType.connectionError:
+        return NetworkException();
+
+      case DioExceptionType.badResponse:
+        return _handleBadResponse(error.response);
+
+      case DioExceptionType.cancel:
+        return ServerException(message: 'Request was cancelled');
+
+      default:
+        return ServerException(message: 'Unexpected error occurred');
+    }
+  }
+
+  static Exception _handleBadResponse(Response? response) {
+    if (response == null) return ServerException(message: 'Unknown server error');
+
+    final statusCode = response.statusCode;
+    final data = response.data;
+    String message = 'Something went wrong';
+
+    if (data is Map && data.containsKey('message')) {
+      message = data['message'];
+    }
+
+    if (statusCode == 401) {
+      return UnauthenticatedException(message: message);
+    }
+
+    if (statusCode == 422 && data is Map) {
+      if (data.containsKey('errors')) {
+        final errors = data['errors'];
+        if (errors is Map && errors.isNotEmpty) {
+          final firstKey = errors.keys.first;
+          final firstErrorList = errors[firstKey];
+          if (firstErrorList is List && firstErrorList.isNotEmpty) {
+            return ServerException(
+              message: firstErrorList.first.toString(),
+              statusCode: 422,
+            );
+          }
+        }
+      }
+    }
+
+    if (statusCode != null && statusCode >= 500) {
+      return ServerException(message: 'Server internal error', statusCode: 500);
+    }
+
+    return ServerException(message: message, statusCode: statusCode);
+  }
+}
