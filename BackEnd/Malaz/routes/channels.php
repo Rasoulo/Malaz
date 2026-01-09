3c316a51e584201c5ab999fa866afd2cec5aed92
maheralnouri244-lab <maheralnouri244@gmail.com>
2026-01-09 17:44:58 +0300

    fix private channels

--- BackEnd/Malaz/routes/channels.php
+++ BackEnd/Malaz/routes/channels.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
+Broadcast::routes(['middleware' => ['auth:sanctum']]);
+
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
 
-
 Broadcast::channel('conversations.{conversationId}', function ($user, $conversationId) {
+
     return \App\Models\Conversation::where('id', $conversationId)
         ->where(function ($q) use ($user) {
             $q->where('user_one_id', $user->id)
